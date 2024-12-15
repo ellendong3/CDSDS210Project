be1b4acbe7d34a4a1fabe9751d142a5294b4876f
Ellen Dong <ezdong220@gmail.com>
2024-12-15 14:56:08 -0500

    Cleaned up some of my code and created a way for the user to see either the entire distribution or just some of the top frequencies

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,60 @@
 use std::fs;
 use std::io;
 use std::collections::HashMap;
-use std::collections::HashSet;
-
 
 
 fn main() {
-    //println!("Hello, world!");
-    let library = readfile();
-    let (degreesmap, degreesvec) = degreetotal(library);
-    let (degreenodes, probs) = degreetonode(degreesmap, degreesvec);
 
-    //println!("{:?}", degreenodes.get(&70));
+    println!("Type 'all' to see the degree distribution of the whole graph or type a positive whole number between 1 and 74 to see the top 1-74 degree frequencies.");
+    let mut counter = 0;
+
+    while counter == 0 {
+        let mut input =String ::new(); 
+        io::stdin().read_line(&mut input).expect("Failed to read line"); 
+        let mut input = input.trim().to_lowercase();
+        if input == "all" {
+            let library = readfile();
+            let (degreesmap, degreesvec) = degreetotal(library);
+            degreetonode(degreesmap, degreesvec, 0);
+            counter += 1;
+        } else {
+            let number: usize = input.parse().expect("Not a integer number");
+            if number >= 1 && number <= 74 {
+                let library = readfile();
+                let (degreesmap, degreesvec) = degreetotal(library);
+                degreetonode(degreesmap, degreesvec, number);
+                counter += 1;
+            } else {
+                println!("Try again. Bad input");
+                io::stdin().read_line(&mut input).expect("Failed to read line"); 
+            }
+        }
+    }
 
 }
 
 fn readfile() -> HashMap<u32, Vec<u32>> {
 
-    let mut cat_ing = HashMap::<String, Vec<String>>::new();
     let mut vec1: Vec<String> = Vec::new();
     let mut vec2: Vec<String> = Vec::new();
 
     let file = fs::read_to_string("src/email-Enron.txt");
+
     for i in file.expect("REASON").split("\r\n") {
         let newstring = i.to_string();
         vec1.push(newstring);
+
     }
 
     for i in 0..vec1.len() {
         if i > 3 && i != (vec1.len() - 1) {
             vec2.push((*vec1[i]).to_string());
+
         }
     }
 
     let mut pairholder: Vec<Vec<u32>> = Vec::new();
+
     for i in &vec2 {
         let holder = i.split("\t");
         let mut pair: Vec<u32> = Vec::new();
@@ -41,22 +62,24 @@ fn readfile() -> HashMap<u32, Vec<u32>> {
         for n in holder {
             let int = n.trim().parse().expect("REASON");
             pair.push(int);
+
         }
         pairholder.push(pair);
 
     }
-
-    //println!("{:?}", pairholder[0][0]);
     
     let mut pairlibrary: HashMap<u32, Vec<u32>> = HashMap::new();
+
     for n in 0..36692 {
         let mut totalsent: Vec<u32> = Vec::new();
         for i in &pairholder {
             if i[0] == n {
                 totalsent.push(i[1]);
             }
+
         }
         pairlibrary.insert(n, totalsent);
+
     }
     
     return pairlibrary
@@ -66,107 +89,83 @@ fn readfile() -> HashMap<u32, Vec<u32>> {
 fn degreetotal(library: HashMap<u32, Vec<u32>>) -> (HashMap<u32, u32>, Vec<u32>) {
     let mut degreelibrary: HashMap<u32, u32> = HashMap::new();
     let mut degreevector: Vec<u32> = Vec::new();
+
     for i in 0..36692 {
         let node = library.get(&i);
         let degrees = node.unwrap().len();
         degreevector.push(degrees as u32);
         degreelibrary.insert(i, degrees.try_into().unwrap());
+
     }
 
     return (degreelibrary, degreevector)
+
 }
 
-fn degreetonode(degreelibrary: HashMap<u32, u32>, vec: Vec<u32>) -> (HashMap<u32, f32>, Vec<f32>) {
+fn degreetonode(degreelibrary: HashMap<u32, u32>, vec: Vec<u32>, number: usize) -> (HashMap<u32, f32>, Vec<f32>) {
     let mut finalprob: HashMap<u32, f32> = HashMap::new();
     let mut degreetonodes: HashMap<u32, u32> = HashMap::new();
     let mut probs: Vec<f32> = Vec::new();
+
     for i in 0..36692 {
         let degrees: u32 = *degreelibrary.get(&i).unwrap() as u32;
         if degreetonodes.get(&degrees) == None {
             degreetonodes.insert(degrees as u32, 1);
+
         } else {
             let key = degreetonodes.entry(degrees).or_insert(0);
             *key += 1;
+
         }
+
     }
+
     let mut uniquevec: Vec<u32> = vec.clone();
     uniquevec.sort();
     uniquevec.dedup();
+
     for i in &uniquevec {
         let key = degreetonodes.entry(*i).or_insert(0);
         let prob = (*key as f32) / (36692 as f32) as f32;
         probs.push(prob);
         finalprob.insert(*i, prob);
+
     }
+    
+    if number == 0 {
+        for i in &uniquevec {
+            let key = finalprob.entry(*i).or_insert(0.0);
+            println!("{:?} degree(s) has a frequency of {:?}", i, key);
 
-    for i in &uniquevec {
-        let key = finalprob.entry(*i).or_insert(0.0);
-        println!("{:?} degree(s) has a frequency of {:?}", i, key);
-    }
-
-    let mut sortedprobs = probs.clone();
-    sortedprobs.sort_by(|a, b| a.partial_cmp(b).unwrap());
-    sortedprobs.reverse();
-
-    println!("---------------------------------");
-
-    for i in 0..25 {
-        let node = probs.iter().position(|&r| r == sortedprobs[i]).unwrap();
-        println!("{:?}. {:?} degrees has a frequency of {:?}", i + 1, node + 1, sortedprobs[i]);
-    }
-
-    return (finalprob, probs)
-}
-
-
-    /*
-
-            for n in holder {
-            let pair: Vec<u32> = Vec::new();
-            let int = n.trim().parse().expect("REASON");
-            pair.push(int);
         }
+    } else if number > 0 {
 
-            for n in i.split("\t") {
-            let int: u32 = n.trim().parse().expect("REASON");
-        }
+        let mut sortedprobs = probs.clone();
+        sortedprobs.sort_by(|a, b| a.partial_cmp(b).unwrap());
+        sortedprobs.reverse();
+        sortedprobs.dedup();
 
-    let mut iter = 0;
-    let mut titles: Vec<String> = Vec::new();
-    let mut ingr: Vec<String> = Vec::new();
+        println!("{:?}", sortedprobs.len());
 
+        for i in 0..number {
+            let index = sortedprobs[i];
 
-    for i in vec1 {
-        for n in i.split(":") {
-            if iter % 2 == 0 {
-                titles.push(n.to_string());
-            } else {
-                ingr.push(n.to_string())
+            for n in &uniquevec {
+                let key = finalprob.entry(*n).or_insert(0.0);
+
+                if *key == index {
+                    println!("{:?}. {:?} degree(s) has a frequency of {:?}", i + 1, n, index);
+
+                }
+
             }
-            iter += 1
+
         }
     }
     
-    let mut cleaningredients: Vec<Vec<String>> = Vec::new();
+    return (finalprob, probs)
 
-    for i in ingr {
-        let mut onerecipe: Vec<String> = Vec::new();
-        for n in i.trim().split(", ") {
-            onerecipe.push(n.to_string());
-        }
-        cleaningredients.push(onerecipe);
-        
-    }
-    //println!("{:?}", cleaningredients);
-
-    for i in 0..(titles.len() - 1) {
-        //println!("{:?}", i);
-        cat_ing.insert(titles[i].clone(), cleaningredients[i].clone());
-        
-    }
-    return cat_ing
-
-    */
+}
 
 
 
